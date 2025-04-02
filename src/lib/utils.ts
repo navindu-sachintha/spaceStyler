6fa641a01a16f70a50c3189ce6e2c95532226c7c
navindu-sachintha <navindu.social@gmail.com>
2025-04-02 10:04:41 +0530

    ✨ Integrate Google Sign-In functionality and update Firebase configuration to use Vite environment variables

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
+import { auth, googleProvider } from "@/config/firebase"
 import { clsx, type ClassValue } from "clsx"
+import { signInWithPopup, signOut } from "firebase/auth"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const signInWithGoogle = async () => {
+  try {
+    await signInWithPopup(auth, googleProvider)
+  } catch (error) {
+    console.error("Error signing in with Google: ", error)
+  }
+}
+
+export const handleSignOut = async () =>{
+  try {
+    await signOut(auth)
+  } catch (error) {
+    console.error("Error signing out: ", error)
+  }
 }
